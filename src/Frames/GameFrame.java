a2ed2d6275fa25ff0eb250f13ba6e787eb13eb81
Stacjonarka <mjwoj@DESKTOP-4R65IHO>
2024-12-24 03:31:27 +0100

    fix removing obstacles

--- src/Frames/GameFrame.java
+++ src/Frames/GameFrame.java
@@ -51,6 +51,7 @@ public class GameFrame extends JFrame {
                         for (Rectangle rect : obstacles) {
                             if (rect.contains(e.getPoint())) {
                                 rectangleClicked = true;
+                                obstacles.remove(rect);
                                 break;
                             }
                         }
